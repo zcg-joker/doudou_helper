63cfd489279faa523d3f9e5bf5c9ab2461258250
zcg-joker <1614030617@qq.com>
2024-08-11 14:39:32 +0800

    no use cuda

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ os.system(f'git clone https://code.openxlab.org.cn/zcgai/xsz_helper.git {base_pa
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+# model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16)
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
