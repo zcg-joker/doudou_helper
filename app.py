5f8c2004d847746afb7f2f6cc9c23cf7a2eeb68b
zcg-joker <1614030617@qq.com>
2024-08-11 13:22:18 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import gradio as gr
+import os
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+
+# download internlm2 to the base_path directory using git tool
+base_path = './internlm2-chat-1_8b-model'
+os.system(f'git clone https://code.openxlab.org.cn/zcgai/xsz_helper.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+
+tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+
+def chat(message,history):
+    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+        yield response
+
+gr.ChatInterface(chat,
+                 title="豆豆的小助手",
+                description="""
+使用 Xtuner 微调了一下子，大概率过拟合了......  
+                 """,
+                 ).queue(1).launch()
